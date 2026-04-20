c57c5b6fc2f4376323a42ddb1b7a38efdbdb5924
Laura Mølnitz <lamo0004@stud.kea.dk>
2026-04-20 16:01:43 +0200

    Lau: login page frontend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+
+  const handleSubmitLogin = async (e: any) => {
+    e.preventDefault();
+
+    console.log(email, password);
+
+    const response = await login(email, password);
+
+    console.log(response);
+  };
+
   return (
     <div>
-      <form action="api/login" method="POST">
-        <input type="text" name="email" id="" placeholder="email" />
-        <input type="password" name="password" id="" placeholder="password" />
+      <form onSubmit={handleSubmitLogin}>
+        <input type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Log in</button>
       </form>
     </div>

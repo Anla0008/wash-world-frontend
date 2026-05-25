425a7de57aa816cf1b5409106a26c5e600cf9190
leamhkea <lehe0001@stud.ek.dk>
2026-05-25 09:25:40 +0200

    Lea: pusher dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import NearestWashCard from "@/components/dashboard/NearestWash";
+import FAQ from "@/components/global/cards/FAQ";
+import SubscriptionCard from "@/components/global/cards/SubscriptionCard";
+import CustomerServiceCard from "@/components/global/cards/CustomerServiceCard";
+import { washData } from "@/mockupData/washData";
+
+const Dashboard = () => {
+  const firstName = localStorage.getItem("user_first_name");
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div>
+        <h1>Hej {firstName}</h1>
+      </div>
+
+      <div>
+        <NearestWashCard />
+      </div>
+
+      <div>
+        <FAQ />
+      </div>
+
+      <div>
+        <h2>Abonnementer</h2>
+        {/* <SubscriptionCard /> */}
+      </div>
+
+      <div>
+        <CustomerServiceCard />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

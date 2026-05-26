e3407fbb753cea1ee57747b5ffb0b1a3cbb7391c
Laura Mølnitz <lamo0004@stud.kea.dk>
2026-05-26 13:14:37 +0200

    Lau: fjernet tid på progress-bar på alt andet end når den bruges til vask process

--- src/components/global/grafik/ProgressBar.tsx
+++ src/components/global/grafik/ProgressBar.tsx
@@ -19,18 +19,13 @@ const ProgressBar = ({ activeIndex, isWashProcess }: ProgressBarProps) => {
     <div className="max-w-100 m-auto w-full">
       {isWashProcess ? (
         <ul className="mb-2 flex px-2.5 justify-between gap-10">
+          w
           {numbers.map((number) => {
             const stepNumber = Number(number);
 
             return (
               <li key={`icon-${number}`} className="flex h-5 justify-center">
-                {stepNumber === 1 ? (
-                  <Card color="foreground" size={20} />
-                ) : stepNumber === 2 ? (
-                  <Wash color="foreground" size={20} />
-                ) : (
-                  <Checkmark color="foreground" size={20} />
-                )}
+                {stepNumber === 1 ? <Card color="foreground" size={20} /> : stepNumber === 2 ? <Wash color="foreground" size={20} /> : <Checkmark color="foreground" size={20} />}
               </li>
             );
           })}
@@ -45,24 +40,13 @@ const ProgressBar = ({ activeIndex, isWashProcess }: ProgressBarProps) => {
           {numbers.map((number) => {
             const stepNumber = Number(number);
 
-            const backgroundClass =
-              stepNumber < activeIndex
-                ? "bg-(--brand-green)"
-                : stepNumber === activeIndex
-                  ? "bg-foreground"
-                  : "bg-background";
+            const backgroundClass = stepNumber < activeIndex ? "bg-(--brand-green)" : stepNumber === activeIndex ? "bg-foreground" : "bg-background";
 
             return (
               <li key={number}>
                 <div className="flex flex-col items-center gap-2">
-                  <div
-                    className={`h-10.5 w-10.5 border-2 border-foreground rounded-full ${backgroundClass}`}
-                  />
-                  {stepNumber === 2 && isFinalStep ? (
-                    <span className="text-sm">
-                      {formattedDuration}
-                    </span>
-                  ) : null}
+                  <div className={`h-10.5 w-10.5 border-2 border-foreground rounded-full ${backgroundClass}`} />
+                  {stepNumber === 2 && isFinalStep && isWashProcess ? <span className="text-sm">{formattedDuration}</span> : null}
                 </div>
               </li>
             );

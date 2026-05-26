6b10b6d5ebf02853bed9da4405922e31d17b02fe
leamhkea <lehe0001@stud.ek.dk>
2026-05-26 14:30:53 +0200

    Lea: Ordnet a href til Link

--- src/components/global/cards/CustomerServiceCard.tsx
+++ src/components/global/cards/CustomerServiceCard.tsx
@@ -1,18 +1,21 @@
 import ArrowRight from "../icons/navigation/ArrowRight";
+import Link from "next/link";
 
 const CustomerServiceCard = () => {
   return (
     <div className="bg-(--gray-10) text-(--foreground-reverse) p-4 rounded-md flex flex-col gap-2">
       <h3 className="extra-bold">Kundeservice</h3>
-      <a href="/damage-report" className="flex justify-between">
+
+      <Link href="/damage-report" className="flex justify-between">
         <span>Skaderapportering</span>
         <ArrowRight size={20} />
-      </a>
+      </Link>
+
       <div className="flex pt-4 gap-1 small text-(--gray-60)">
         <p>Kontakt os:</p>
-        <a href="mailto:kundeservice@washworld.dk" className="underline">
+        <Link href="mailto:kundeservice@washworld.dk" className="underline">
           kundeservice@washworld.dk
-        </a>
+        </Link>
       </div>
     </div>
   );
